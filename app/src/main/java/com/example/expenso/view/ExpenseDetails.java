059c0482638cb835b061ba199c38426d07394b5c
intel <adityakhaladkar98@gmal.com>
2022-11-07 18:25:13 +0530

    added expense detail UI

--- app/src/main/java/com/example/expenso/view/ExpenseDetails.java
+++ app/src/main/java/com/example/expenso/view/ExpenseDetails.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 import com.example.expenso.R;
 
 public class ExpenseDetails extends AppCompatActivity {
-    TextView detail_name, detail_price, detail_date, detail_description;
+    TextView   detail_name,  detail_price,  detail_date,  detail_description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,18 +16,18 @@ public class ExpenseDetails extends AppCompatActivity {
         setContentView(R.layout.activity_expense_details);
 
 
-        detail_name = findViewById(R.id.detail_name);
-        detail_price = findViewById(R.id.detail_price);
-        detail_date = findViewById(R.id.detail_date);
-        detail_description = findViewById(R.id.detail_description);
+        detail_name =   findViewById(R.id.detail_name);
+        detail_price =  findViewById(R.id.detail_price);
+        detail_date =  findViewById(R.id.detail_date);
+        detail_description =  findViewById(R.id.detail_description);
 
-        String name = getIntent().getStringExtra("name");
-        int price = getIntent().getIntExtra("price", 0);
-        String date = getIntent().getStringExtra("date");
-        String description = getIntent().getStringExtra("description");
+        String name = getIntent().getStringExtra("  name");
+        int price = getIntent().getIntExtra("  price", 0);
+        String date = getIntent().getStringExtra(" date");
+        String description = getIntent().getStringExtra(" description");
 
         detail_name.setText(name);
-        detail_price.setText("Rs: " + String.valueOf(price));
+        detail_price.setText("Rs: " + String.valueOf(price) + " /");
         detail_date.setText(date);
         detail_description.setText(description);
     }
