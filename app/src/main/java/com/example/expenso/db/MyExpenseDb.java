0a53001e86d740585702006294c71544dd2c8787
intel <adityakhaladkar98@gmal.com>
2022-11-07 18:01:47 +0530

    added database class, defined version and created class for database model, defined getters and setters

--- app/src/main/java/com/example/expenso/db/MyExpenseDb.java
+++ app/src/main/java/com/example/expenso/db/MyExpenseDb.java
@@ -5,7 +5,12 @@ import androidx.room.RoomDatabase;
 
 import com.example.expenso.model.ExpenseModel;
 
-@Database(entities = {ExpenseModel.class}, version = 1)
+@Database(
+        entities = {
+                ExpenseModel.class
+        },
+        version = 1
+)
 public abstract class MyExpenseDb extends RoomDatabase {
     public abstract MyExpenseDao myExpenseDao();
 }
--- app/src/main/java/com/example/expenso/model/ExpenseModel.java
+++ app/src/main/java/com/example/expenso/model/ExpenseModel.java
@@ -4,7 +4,9 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-@Entity(tableName = "my_expense")
+@Entity(
+        tableName = "my_expense"
+)
 public class ExpenseModel {
     @PrimaryKey(autoGenerate = true)
     public int id;
